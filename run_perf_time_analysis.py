966530d2250243f8b39ccd368de09126615095b5
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2016-02-10 19:57:49 +0300

    Update stats calculation

--- run_perf_time_analysis.py
+++ run_perf_time_analysis.py
@@ -1,13 +1,12 @@
 import argparse
-import unittest
-from collections import namedtuple
-from itertools import groupby, takewhile
-import xml.etree.ElementTree as ET
-
-import numpy as np
-
 import os
 import re
+import unittest
+import xml.etree.ElementTree as ET
+from collections import namedtuple
+from itertools import groupby, takewhile
+
+import numpy as np
 import xmlrunner
 from elasticsearch import Elasticsearch
 
@@ -66,17 +65,17 @@ def get_stats_data(url, days):
     def get_name(record):
         return record['name_with_test'] if record['metric_type'] == 'http_metric' else record['name']
 
-    return map(lambda r: StatRecord(branch=get_branch(r['_source']),
-                                    name=get_name(r['_source']),
-                                    timestamp=r['_source']['timestamp'],
-                                    value=r['_source']['median']),
-               search_results)
+    return sorted(map(lambda r: StatRecord(branch=get_branch(r['_source']),
+                                           name=get_name(r['_source']),
+                                           timestamp=r['_source']['datetime'],
+                                           value=r['_source']['median']),
+                      search_results), key=lambda r: r.timestamp)
 
 
 def get_moving_averages(stats_data, window):
     for branch, branch_records in groupby(sorted(stats_data, key=lambda r: r.branch), lambda r: r.branch):
         for key, test_records in groupby(sorted(branch_records, key=lambda r: r.name), lambda r: r.name):
-            values = map(lambda r: r.value, sorted(test_records, key=lambda r: r.timestamp))
+            values = map(lambda r: r.value, test_records)
 
             if len(values) < window * 2:
                 continue
@@ -88,15 +87,14 @@ def get_moving_averages(stats_data, window):
 
 # unit tests
 
-def generate_test_classes(moving_averages):
+def generate_test_classes(moving_averages, window):
     def get_class(branch, moving_averages):
         class TestSequenceMeta(type):
             def __new__(mcs, name, bases, dict):
                 def gen_test_instant_raising_trend(moving_average):
                     def test(self):
-                        # last build raising trend?
                         trend_percent = trend(moving_average.value) * 100
-                        instant_threshold = 3
+                        instant_threshold = 2.5
 
                         self.assertLessEqual(trend_percent, instant_threshold,
                                              'Instant performance degradation for "{test_name}" is {percent:3.2f}%'.format(
@@ -106,13 +104,12 @@ def generate_test_classes(moving_averages):
 
                 def gen_test_long_raising_trend(moving_average):
                     def test(self):
-                        # long raising trend?
                         deltas = zip(moving_average.value, moving_average.value[1:])
                         raising_trend = reversed(list(takewhile(lambda (prv, nxt): nxt > prv, reversed(deltas))))
                         data = list(raising_trend)
                         if len(data) > 0:
-                            long_threshold = 5
-                            long_trend_percent = trend([data[0][0], data[-1][1]]) * 100
+                            long_threshold = 6 if not moving_average.name.startswith('notifications') else 20
+                            long_trend_percent = trend([min(moving_average.value[-window:]), data[-1][1]]) * 100
 
                             self.assertLessEqual(long_trend_percent, long_threshold,
                                                  'Long time performance degradation for "{test_name}" is {percent:3.2f}%'.format(
@@ -178,13 +175,13 @@ def merge_test_results(output_file):
 # stats calculations
 
 def simple_moving_average(values, window):
-    weights = np.repeat(1.0, window) / window
+    weights = np.repeat(-.5, window) / window
 
     return np.convolve(values, weights, 'valid')
 
 
 def exponential_moving_average(values, window):
-    weights = np.ma.exp(np.linspace(-.5, 0, window))
+    weights = np.ma.exp(np.linspace(1, 0, window))
     weights /= weights.sum()
 
     return np.convolve(values, weights)[window - 1: len(values)]
@@ -208,6 +205,6 @@ if __name__ == '__main__':
     raw_stats = get_stats_data(args.url, args.days)
     moving_averages = get_moving_averages(raw_stats, args.window)
 
-    classes = generate_test_classes(moving_averages)
+    classes = generate_test_classes(moving_averages, args.window)
 
     run_tests(classes, args.merge_file)
