8cd583a24d6d72ded02e513b2aa2135aa64e60e1
Dmitry Tsarevich <tsarevich@targetprocess.com>
2018-12-17 12:46:27 +0300

    Redirect performance degradation notifications to #tp-performance
    Do not flood #tp-development.

--- run_analyzer.bat
+++ run_analyzer.bat
@@ -1,4 +1,4 @@
 call scripts\setup-environment.bat
 .env\Scripts\python.exe src\run_perf_time_analysis.py -mf "%1" --es-host "%4" --es-region "%5" --es-access-key-id "%6" --es-secret-access-key "%7"
-.env\Scripts\python.exe src\notify_slack.py -f "%1" --slack-notification-url "%2" --slack-channel "#tp-development" --build-url "%3"
+.env\Scripts\python.exe src\notify_slack.py --file "%1" --slack-notification-url "%2" --slack-channel "#tp-performance" --build-url "%3"
 call scripts\cleanup-environment.bat
